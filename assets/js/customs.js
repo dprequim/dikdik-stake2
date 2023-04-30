ebedbc6b59482b1e9bb9a26d52eb14a937d7ea25
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-04-30 11:17:48 +0800

    drawer update

---
+++ assets/js/customs.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-btn').addEventListener('click', function() {
+    document.getElementsByClassName('drawer')[0].classList.toggle('open');
+    // document.querySelector('#toggle-btn i').classList.toggle('open');
+  });
